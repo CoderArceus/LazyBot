389e6a51b0ffc99ecd290b16fd00dc312a01eedd
CoderArceus <75372901+CoderArceus@users.noreply.github.com>
2021-03-12 22:18:20 +0530

    Add files via upload

--- lazy.py
+++ lazy.py
@@ -1,26 +1,21 @@
-# IMPORT DISCORD.PY. ALLOWS ACCESS TO DISCORD'S API.
 import discord
-
-# IMPORT THE OS MODULE.
 import os
-
-# IMPORT LOAD_DOTENV FUNCTION FROM DOTENV MODULE.
+import time
 from dotenv import load_dotenv
-
-# IMPORT COMMANDS FROM THE DISCORD.EXT MODULE.
 from discord.ext import commands
 import random
-
-# LOADS THE .ENV FILE THAT RESIDES ON THE SAME LEVEL AS THE SCRIPT.
+intents = discord.Intents.default()
+intents.members = True
 load_dotenv()
 
-# GRAB THE API TOKEN FROM THE .ENV FILE.
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
-
 bot = discord.Client()
+bot = commands.Bot(command_prefix="$", intents=intents)
+bot.remove_command("help")
 
-# CREATES A NEW BOT OBJECT WITH A SPECIFIED PREFIX. IT CAN BE WHATEVER YOU WANT IT TO BE.
-bot = commands.Bot(command_prefix="$")
+@bot.event
+async def on_ready():
+	await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="$hp"))
 
 @bot.command(name="std")
 async def shutdown(ctx):
@@ -31,59 +26,78 @@ async def shutdown(ctx):
     else:
         await ctx.send("You dont have sufficient permissions to perform this action!")
     
-@bot.command(name="ping")
-async def ping_da_bot(ctx):
-	await ctx.channel.send("pong")
+@bot.command(aliases = ['Ping', 'ping'])
+async def pinger(ctx):
+     embed = discord.Embed(description = f':hourglass: Pong!  {round(bot.latency * 1000)}ms', color = 0x00FFFF)
+     await ctx.send(embed = embed)
 
-@bot.command(name="bd")
+@bot.command(aliases = ['bold', 'bd', 'Bold'])
 async def bold_da_text(ctx, arg):
 	await ctx.channel.send("**" + arg +"**")
 	
-@bot.command(name="it")
+@bot.command(aliases = ['italic', 'it', 'Italic'])
 async def italic_da_text(ctx, arg):
 	await ctx.channel.send("*" + arg +"*")
 	
-@bot.command(name="hp")
+@bot.command(name="help")
 async def commands(ctx):
 	embed = discord.Embed(title = "LazyBot Commands" , description = """```
+--> <> are required fiels whereas [ ] are optional
 
-1. ping
+1. Ping
 Use to check if bot is active.
 
-2. bd <text>
+2. Bold <text>
 Prints text in bold.
+--> Alias Bd
 
-3. it <text>
+3. Italic <text>
 Prints text in italics.
+--> Alias It
 
-4. hp
+4. Help
 Shows this menu.
 
-5. hello
+5. Hello [user]
 Says hello.
 
-6. hru
+6. Hru
 I'm lazy.
 
-7. f
+7. F
 FFFFFFF.
 
-8. random <num1> <num2>
+8. Random <num1> <num2>
 Chooses a random number between two numbers.
+
+9. Coinflip
+Flips a coin.
+
+11. Welcome [user]
+Welcomes.
 
 BOT PREFIX ->  $```""", color = 0x00FFFF)
 	await ctx.channel.send(embed = embed)
 
-@bot.command(name="hello")
-async def hello(ctx):
-	await ctx.send(f"Hello Ji, {ctx.author.name}")
+@bot.command(aliases = ['Hello', 'hello'])
+async def hello_ji(ctx, user: discord.Member=None):
+	if not user:
+		await ctx.send(f"Hello Ji, {ctx.author.name}")
+		
+	else:
+		await ctx.send("""
+
+░█─░█ █▀▀ █── █── █▀▀█ 
+░█▀▀█ █▀▀ █── █── █──█ 
+░█─░█ ▀▀▀ ▀▀▀ ▀▀▀ ▀▀▀▀
+||{}||""".format(user.mention))
 	
-@bot.command(name="hru")
+@bot.command(aliases = ['Hru', 'hru'])
 async def howareyou(ctx):
 	await ctx.channel.send("I'm tired af but my owner doesn't let me sleep :pensive:")
 
-@bot.command(name="f")
-async def F(ctx):
+@bot.command(aliases = ['F', 'f'])
+async def F_respects(ctx):
 	await ctx.channel.send("""
 FFFFFFFFFFFFFF
 FFF
@@ -92,17 +106,17 @@ FFFFFFFFF
 FFF
 FFF
 FFF
-FFF""", color = 0x00FFFF)
+FFF""")
 
-@bot.command(name="random")
+@bot.command(aliases = ['Random', 'random'])
 async def randomizer(ctx, num1, num2):
 	num1final = int(num1)
 	num2final = int(num2)
 	embed = discord.Embed(title = f"Random Number Between {num1final} and {num2final}", description = random.randint(num1final, num2final), color = 0x00FFFF)
 	await ctx.send(embed = embed)
 	
-@bot.command(name="coinflip")
-async def coinflip(ctx):
+@bot.command(aliases = ['Coinflip', 'coinflip'])
+async def flip_da_coin(ctx):
 	variable = [
 	"Heads",
 	"Tails"]
@@ -110,5 +124,50 @@ async def coinflip(ctx):
 	embed = discord.Embed(title = "Coinflipper!", description = f"The Coin has flipped on.... {random.choice(variable)}", color = 0x00FFFF)
 	await ctx.send(embed = embed)
 	
-# EXECUTES THE BOT WITH THE SPECIFIED TOKEN. TOKEN HAS BEEN REMOVED AND USED JUST AS AN EXAMPLE.
+@bot.command(aliases = ['Welcome', 'welcome'])
+async def welcomer_smol(ctx, user: discord.Member=None):
+	if not user:
+		await ctx.send("""
+───▄▀▀▀▄▄▄▄▄▄▄▀▀▀▄───
+───█▒▒░░░░░░░░░▒▒█───
+────█░░█░░░░░█░░█────
+─▄▄──█░░░▀█▀░░░█──▄▄─
+█░░█─▀▄░░░░░░░▄▀─█░░█
+█▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█
+█░░╦─╦╔╗╦─╔╗╔╗╔╦╗╔╗░░█
+█░░║║║╠─║─║─║║║║║╠─░░█
+█░░╚╩╝╚╝╚╝╚╝╚╝╩─╩╚╝░░█
+█▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█
+""")
+	else:
+		await ctx.send("""
+───▄▀▀▀▄▄▄▄▄▄▄▀▀▀▄───
+───█▒▒░░░░░░░░░▒▒█───
+────█░░█░░░░░█░░█────
+─▄▄──█░░░▀█▀░░░█──▄▄─
+█░░█─▀▄░░░░░░░▄▀─█░░█
+█▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█
+█░░╦─╦╔╗╦─╔╗╔╗╔╦╗╔╗░░█
+█░░║║║╠─║─║─║║║║║╠─░░█
+█░░╚╩╝╚╝╚╝╚╝╚╝╩─╩╚╝░░█
+█▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█
+||{}||""".format(user.mention))
+
+@bot.event
+async def on_member_join(member):
+	await bot.get_channel(799616295364198403).send(f"""
+───▄▀▀▀▄▄▄▄▄▄▄▀▀▀▄───
+───█▒▒░░░░░░░░░▒▒█───
+────█░░█░░░░░█░░█────
+─▄▄──█░░░▀█▀░░░█──▄▄─
+█░░█─▀▄░░░░░░░▄▀─█░░█
+█▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█
+█░░╦─╦╔╗╦─╔╗╔╗╔╦╗╔╗░░█
+█░░║║║╠─║─║─║║║║║╠─░░█
+█░░╚╩╝╚╝╚╝╚╝╚╝╩─╩╚╝░░█
+█▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█
+
+{member.mention} to {member.guild}""")
+
+
 bot.run(DISCORD_TOKEN)
